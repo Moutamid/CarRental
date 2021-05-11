f1ddf79f6a76b42b7e18de7d84ada20def87efff
Moutamid <lumianokia188@gmail.com>
2021-05-12 02:20:01 +0500

    5 12 2021

---
+++ app/src/main/java/com/moutamid/carrentalproject/BottomNavigationActivity.java
@@ -0,0 +1,190 @@
+package com.moutamid.carrentalproject;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.Scroller;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.tbuonomo.viewpagerdotsindicator.SpringDotsIndicator;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+import me.ibrahimsn.lib.OnItemSelectedListener;
+import me.ibrahimsn.lib.SmoothBottomBar;
+
+public class BottomNavigationActivity extends AppCompatActivity {
+    private static final String TAG = "BottomNavigationActivit";
+
+    private ViewPagerFragmentAdapter adapter;
+    private ViewPager viewPager;
+    private SmoothBottomBar smoothBottomBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        setContentView(R.layout.activity_bottom_navigation);
+
+        viewPager = findViewById(R.id.bottom_navigation_view_pager);
+        smoothBottomBar = findViewById(R.id.bottom_navigation_Bar);
+
+        adapter = new ViewPagerFragmentAdapter(getSupportFragmentManager());
+
+        // Setting up the view Pager
+        setupViewPager(viewPager);
+
+        smoothBottomBar.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public boolean onItemSelect(int i) {
+
+                viewPager.setCurrentItem(i, true);
+
+                return false;
+            }
+        });
+
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+
+        // Adding Fragments to Adapter
+        adapter.addFragment(new HomeFragment());
+        adapter.addFragment(new FragmentOnBoardingTwo());
+        adapter.addFragment(new FragmentOnBoardingThree());
+        adapter.addFragment(new FragmentOnBoardingThree());
+
+        // Setting Adapter To ViewPager
+        viewPager.setOffscreenPageLimit(4);
+        viewPager.setAdapter(adapter);
+
+        Log.d(TAG, "setupViewPager: adapter attached");
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float v, int i1) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+                switch (position) {
+                    case 0:
+                    case 1:
+                    case 2:
+                    case 3:
+                        smoothBottomBar.setItemActiveIndex(position);
+                        break;
+                    default:
+                        Log.d(TAG, "onPageSelected: default occurred");
+                        break;
+                }
+
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (viewPager.getCurrentItem() == 0) super.onBackPressed();
+
+        else viewPager.setCurrentItem(viewPager.getCurrentItem() - 1);
+
+    }
+
+    public static class NonSwipableViewPager extends ViewPager {
+
+
+        public NonSwipableViewPager(@NonNull Context context) {
+            super(context);
+        }
+
+        public NonSwipableViewPager(@NonNull Context context, @Nullable AttributeSet attrs) {
+            super(context, attrs);
+        }
+
+        @Override
+        public boolean onInterceptTouchEvent(MotionEvent ev) {
+            return false;
+        }
+
+        @Override
+        public boolean onTouchEvent(MotionEvent ev) {
+            return false;
+        }
+
+        public void setMyScroller() {
+
+            try {
+                Class<?> viewpager = ViewPager.class;
+                Field scroller = viewpager.getDeclaredField("mScroller");
+                scroller.setAccessible(true);
+                scroller.set(this, new MyScroller(getContext()));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        public class MyScroller extends Scroller {
+
+            public MyScroller(Context context) {
+                super(context, new DecelerateInterpolator());
+            }
+
+            @Override
+            public void startScroll(int startX, int startY, int dx, int dy, int duration) {
+                super.startScroll(startX, startY, dx, dy, 350);
+            }
+        }
+    }
+
+    public static class ViewPagerFragmentAdapter extends FragmentStatePagerAdapter {
+
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+
+        public void addFragment(Fragment fragment) {
+            mFragmentList.add(fragment);
+        }
+
+        public ViewPagerFragmentAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/moutamid/carrentalproject/HomeFragment.java
@@ -0,0 +1,36 @@
+package com.moutamid.carrentalproject;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class HomeFragment extends Fragment {
+
+    private View view;
+
+//    public LinearLayout spaceLayout() {
+//
+//        return view.findViewById(R.id.textonboardingspace);
+//
+//    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.home_fragment, container, false);
+
+//        ImageView progress = view.findViewById(R.id.welcome_imageView);
+//
+//        AnimationDrawable frameAnimation = (AnimationDrawable) progress.getDrawable();
+//        frameAnimation.setCallback(progress);
+//        frameAnimation.setVisible(true, true);
+//        frameAnimation.start();
+
+        return view;
+    }
+}
